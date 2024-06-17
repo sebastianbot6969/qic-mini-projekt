5f7a84ec72e9c2cd3225488cb1f20bd24e46aa2e
Sebastian <72598707+sebastianbot6969@users.noreply.github.com>
2024-06-17 22:17:16 +0200

    Update hardware_benchmark.py

--- hardware_benchmark.py
+++ hardware_benchmark.py
@@ -3,22 +3,32 @@ import azure.quantum
 from azure.quantum.qiskit import AzureQuantumProvider
 from azure.quantum import Workspace
 import json
+import pandas as pd
+
 
 if __name__ == "__main__":
-    with open('resource.json') as resource_file:
+        with open('resource.json') as resource_file:
         resource = json.load(resource_file)
-    workspace = Workspace(
+        workspace = Workspace(
         resource_id=resource['id'],
         location=resource['location']
-    )
-    provider = AzureQuantumProvider(workspace)
-    backend = provider.get_backend("rigetti.sim.qvm")
+        )
+        provider = AzureQuantumProvider(workspace)
+        backend = provider.get_backend("rigetti.sim.qvm")
+        dataframe = pd.DataFrame(columns=['Data Length', 'Element'])
 
-    num_tests = 1
-    for test in range(num_tests):
         data, element = setup(10)
         a = grovers_algorithm(data, element, backend)
-        print("Test:", test + 1)
-        print("Data:", data)
-        print("Element:", element)
+
+        result = sampler.run(a, shots=10000).result()
+
+        counts = result.get_counts(a)
+        plot_distribution(counts).show()
+
+        max_index_length = len(bin(len(data) - 1)[2:])
+        marked_index = data.index(element)
+        marked_state = format(marked_index, f'0{max_index_length}b')
+
+        dataframe.loc[len(dataframe)] = [len(data), marked_state]
+        dataframe.to_csv('test_data.csv', index=False)
 
